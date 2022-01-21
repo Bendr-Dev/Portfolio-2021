b8896d2bb8714b5332f9b4e14d269aa623700e6f
Brandon Endres <17bendres@gmail.com>
2022-01-21 12:01:36 -0500

    Adds animation function

--- src/animation.ts
+++ src/animation.ts
@@ -6,13 +6,20 @@
  * @param duration (number): Length of easing
  * @returns updated position value based off time
  */
-const cubicEasingInOut = (currentTime: number, startValue: number, change: number, duration: number): number => {
-    currentTime /= duration / 2;
-    if (currentTime < 1) {
-        return change / 2 * currentTime * currentTime * currentTime + startValue;
-    }
-    currentTime -= 2;
-    return change / 2 * (currentTime * currentTime * currentTime + 2) + startValue;
+const cubicEasingInOut = (
+  currentTime: number,
+  startValue: number,
+  change: number,
+  duration: number
+): number => {
+  currentTime /= duration / 2;
+  if (currentTime < 1) {
+    return (change / 2) * currentTime * currentTime * currentTime + startValue;
+  }
+  currentTime -= 2;
+  return (
+    (change / 2) * (currentTime * currentTime * currentTime + 2) + startValue
+  );
 };
 
 /**
@@ -22,25 +29,128 @@ const cubicEasingInOut = (currentTime: number, startValue: number, change: numbe
  * @param oldPosition (number): Displacement (by percentage) of element leaving
  * @param newPosition (number): Displacement (by percentage) of element going to
  */
-export const translateElements = (sections: Array<HTMLElement>, duration: number, oldPosition: number, newPosition: number): void => {
-    let currentTimestamp: number | null = null;
+export const translateElements = (
+  sections: Array<HTMLElement>,
+  duration: number,
+  oldPosition: number,
+  newPosition: number
+): void => {
+  let currentTimestamp: number | null = null;
 
-    function frame(timestamp: number) {
-        if (!currentTimestamp) {
-            currentTimestamp = timestamp; 
-        }
-
-        const elapsed = timestamp - currentTimestamp;
-
-        const nextPosition = Math.min(cubicEasingInOut(elapsed, oldPosition, newPosition - oldPosition, duration), 0);
-        sections.forEach((section: HTMLElement) => {
-            section.style.transform = `translateY(${nextPosition}%)`;
-        });
-
-        if (elapsed < duration) {
-            window.requestAnimationFrame(frame);
-        }
+  function frame(timestamp: number) {
+    if (!currentTimestamp) {
+      currentTimestamp = timestamp;
     }
 
-    window.requestAnimationFrame(frame);
+    const elapsed = timestamp - currentTimestamp;
+
+    const nextPosition = Math.min(
+      cubicEasingInOut(
+        elapsed,
+        oldPosition,
+        newPosition - oldPosition,
+        duration
+      ),
+      0
+    );
+    sections.forEach((section: HTMLElement) => {
+      section.style.transform = `translateY(${nextPosition}%)`;
+    });
+
+    if (elapsed < duration) {
+      window.requestAnimationFrame(frame);
+    }
+  }
+
+  window.requestAnimationFrame(frame);
+};
+
+export const animateCanvas = (
+  canvas: HTMLCanvasElement,
+  numberOfStars: number,
+  parentElement: HTMLElement
+) => {
+  canvas.style.width = `${parentElement.offsetWidth}px`;
+  canvas.style.height = `${parentElement.offsetHeight}px`;
+  canvas.width = parentElement.offsetWidth;
+  canvas.height = parentElement.offsetHeight;
+  const randomInt = (max: number, min: number): number => {
+    return Math.floor(Math.random() * (max - min) + min);
+  };
+  const colors = [
+    "#FF5400",
+    "#0077B6",
+    "#FF6D00",
+    "#0096C7",
+    "#FF8500",
+    "#00B4D8",
+  ];
+  const stars = Array<Star>();
+  const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+  class Star {
+    private x: number;
+    private y: number;
+    private radius: number;
+    private deltaX: number;
+    private color: string;
+
+    constructor(x?: number, y?: number, radius?: number, color?: string) {
+      this.x = x || randomInt(canvas.offsetWidth, 0);
+      this.y = y || randomInt(canvas.offsetHeight, 0);
+      this.radius = radius || Math.random() * 1.1;
+      this.color = color || colors[randomInt(colors.length - 1, 0)];
+      this.deltaX = Math.random() * 0.3;
+    }
+
+    draw() {
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+      ctx.shadowBlur = randomInt(12, 3);
+      ctx.shadowColor = this.color;
+      ctx.strokeStyle = this.color;
+      ctx.fillStyle = this.color;
+      ctx.fill();
+      ctx.stroke();
+      ctx.closePath();
+    }
+
+    createNewStar(stars: Array<Star> = Array<Star>()) {
+      const index = stars.indexOf(this);
+      stars.splice(index, 1);
+      stars.push(new Star(-5));
+    }
+
+    update(stars: Array<Star> = Array<Star>()) {
+      this.x - this.radius > canvas.offsetWidth && this.createNewStar(stars);
+      this.x += this.deltaX;
+      this.draw();
+    }
+  }
+
+  const initialize = () => {
+    for (let index = 0; index < numberOfStars; index++) {
+      stars.push(new Star());
+    }
+  };
+
+  window.addEventListener("resize", () => {
+    canvas.style.width = `${parentElement.offsetWidth}px`;
+    canvas.style.height = `${parentElement.offsetHeight}px`;
+    canvas.width = parentElement.offsetWidth;
+    canvas.height = parentElement.offsetHeight;
+    stars.splice(0, stars.length);
+    initialize();
+  });
+
+  function frame() {
+    ctx.clearRect(0, 0, canvas.offsetWidth, canvas.offsetHeight);
+    ctx.fillStyle = "#00001e";
+    ctx.fillRect(0, 0, canvas.offsetWidth, canvas.offsetHeight);
+    stars.forEach((star: Star) => star.update(stars));
+    requestAnimationFrame(frame);
+  }
+
+  initialize();
+  requestAnimationFrame(frame);
 };
